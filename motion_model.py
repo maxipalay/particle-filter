23612e9c6d58eb72e66409266149377ac2207181
Maximiliano Palay <maximilianopalay@gmail.com>
2023-10-05 17:05:43 -0500

    motion model docstring and comments

--- motion_model.py
+++ motion_model.py
@@ -9,6 +9,15 @@ SIGMA_W = 2.0
 
 
 def motion_model(state, control, dt):
+    """Function that represents a motion model. 
+        Input args:
+            :state: current state
+            :control: current control
+            :dt: time for which the control is applied (dt)
+        Output:
+            Predicted state at time t+dt
+    """
+    # just for ease of understanding explicitly declare the variables
     v = control[Control.V]
     w = control[Control.W]
     x0 = state[State.X]
@@ -19,6 +28,7 @@ def motion_model(state, control, dt):
     v = v + sample(SIGMA_V)
     w = w + sample(SIGMA_W)
 
+    # the actual motion model
     x = x0 + v * np.cos(theta0) * dt
     y = y0 + v * np.sin(theta0) * dt
     theta = theta0 + w * dt
