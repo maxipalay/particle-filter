821ba6bb09a7ddc513b30c31e6c243c4bb9d9702
Maximiliano Palay <maximilianopalay@gmail.com>
2023-10-05 17:28:02 -0500

    particle filter docstrings and comments

--- particle_filter.py
+++ particle_filter.py
@@ -1,41 +1,38 @@
 import numpy as np
 from prob_utils import low_variance_sampler
 from data_utils import Control
+from state import State
+
 
 class ParticleFilter:
     """Class representing a Particle Filter.
         Args:
             :motion_model: a function that given a state sample, a control and a dt, returns a new state sample
             :measurement_likelihood: a function that given a state sample and a list of landmarks, calculates a score for the likelihood of landmarks observation
+            :initial_state: the state for the first run of the particle filter
             :n_particles: the number of particles to use
-            :config_length:
     """
 
-    def __init__(self, motion_model, measurement_likelihood, initial_state, n_particles, config_length=3):
-        """Args:
-            :measurement_model:
-            :motion_model:
-            :initial_state:
-        """
+    def __init__(self, motion_model, measurement_likelihood, initial_state, n_particles):
         self._n_particles = n_particles
-        self._config_length = config_length
         self._state = initial_state
-        # the two functions needed by the filter
         self._motion_model = motion_model
         self._measurement_likelihood = measurement_likelihood
 
     def update(self, control, measurements, dt, map):
-        """Update our state.
+        """Update the state.
             Input args:
-                :control:
-                :measurements:
-                :dt:
+                :control: current control inputs
+                :measurements: list of current measurements
+                :dt: time for which the controls are applied
+            Output:
+                new state
         """
         # if there is a control command or measurement, run the filter
         if control[Control.V] != 0.0 or control[Control.W] != 0.0 or measurements.size > 0:
             # initialize variables
-            x, x_hat, weights = np.zeros((self._n_particles, self._config_length)), np.zeros(
-                (self._n_particles, self._config_length)), np.zeros(self._n_particles)
+            x, x_hat, weights = np.zeros((self._n_particles, State.LENGTH)), np.zeros(
+                (self._n_particles, State.LENGTH)), np.zeros(self._n_particles)
             # for each particle
             for m in range(self._n_particles):
                 # get a sample from the motion model
